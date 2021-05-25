e2e9a65052d48db1b14805d5ff1dea3dc5fb1c26
BoBoBalloon <61363161+BoBoBalloon@users.noreply.github.com>
2021-05-25 18:28:38 -0400

    added dependencies + added listener for "Seeking Revenge" quest by bongots

--- src/main/java/com/runicrealms/runicquests/Plugin.java
+++ src/main/java/com/runicrealms/runicquests/Plugin.java
@@ -13,6 +13,7 @@ import com.runicrealms.runicquests.event.EventPlayerJoinQuit;
 import com.runicrealms.runicquests.event.EventPlayerLocation;
 import com.runicrealms.runicquests.event.custom.RightClickNpcHandler;
 import com.runicrealms.runicquests.listeners.JournalListener;
+import com.runicrealms.runicquests.listeners.questwriterlisteners.AncientWhalePowderListener;
 import com.runicrealms.runicquests.passivenpcs.PassiveNpcClickListener;
 import com.runicrealms.runicquests.passivenpcs.PassiveNpcHandler;
 import com.runicrealms.runicquests.quests.FirstNpcState;
@@ -71,6 +72,10 @@ public class Plugin extends JavaPlugin {
 		this.getServer().getPluginManager().registerEvents(new RightClickNpcHandler(), this);
 		this.getServer().getPluginManager().registerEvents(holoManager, this);
 		this.getServer().getPluginManager().registerEvents(new PassiveNpcClickListener(), this);
+
+		//quest writer listeners
+		this.getServer().getPluginManager().registerEvents(new AncientWhalePowderListener(), this);
+
 		for (Player player : Bukkit.getOnlinePlayers()) { // Loop through online players (fixes bug with /reload)
 			if (CharacterApi.getCurrentCharacterSlot(player) != null) {
 				EventPlayerJoinQuit.runJoinEvent(player, CharacterApi.getCurrentCharacterSlot(player)); // Read PlayerJoinQuitEvent.runJoinEvent
