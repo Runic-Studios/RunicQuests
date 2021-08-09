68d3d0b8de34395d134021ec203501c958142548
Dimetrius Hightower <dimetrius.hightower@gmail.com>
2021-08-08 21:05:39 -0700

    updated quest menu!

--- src/main/java/com/runicrealms/runicquests/event/EventInventory.java
+++ src/main/java/com/runicrealms/runicquests/event/EventInventory.java
@@ -21,14 +21,16 @@ import java.util.*;
 
 public class EventInventory implements Listener {
 
-    private static final int INVENTORY_SIZE = 54;
+    private static final int INVENTORY_SIZE = 45;
+    private static final int QUEST_INVENTORY_FIRST_INDEX = 9;
     private static final String QUEST_MENU_TITLE = ChatColor.GOLD + "Quests";
     private static final Map<UUID, QuestMenuWrapper> playersInQuestGui = new HashMap<>();
 
     /**
+     * This menu displays a list of available quests to the player
      *
-     * @param player
-     * @param page
+     * @param player to display gui to
+     * @param page   of the quest menu to display
      */
     public static void openQuestGui(Player player, Integer page) {
         boolean showRepeatableQuests = playersInQuestGui.get(player.getUniqueId()) != null && playersInQuestGui.get(player.getUniqueId()).isShowingRepeatableQuests();
@@ -38,12 +40,19 @@ public class EventInventory implements Listener {
         items.put(4, infoPaper());
         items.put(5, toggleShowRepeatableQuestsItem());
         items.put(8, forwardArrow());
-        for (int i = ((page - 1) * (INVENTORY_SIZE - 1)) + 9; i < page * INVENTORY_SIZE; i++) {
-            if (i + 1 <= quests.size()) {
-                items.put((i - (page - 1) * (INVENTORY_SIZE - 1)), quests.get(i).generateQuestIcon(player));
+
+        int location = (page - 1) * INVENTORY_SIZE; // holds our place in the list
+        try {
+            for (int i = 0; i < INVENTORY_SIZE; i++) {
+                if ((location + i) < quests.size()) {
+                    items.put(i + QUEST_INVENTORY_FIRST_INDEX, quests.get((location + i)).generateQuestIcon(player));
+                }
             }
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
         }
-        Inventory inventory = Bukkit.createInventory(null, INVENTORY_SIZE,
+
+        Inventory inventory = Bukkit.createInventory(null, INVENTORY_SIZE + QUEST_INVENTORY_FIRST_INDEX,
                 ChatColor.translateAlternateColorCodes('&', QUEST_MENU_TITLE + " (pg. " + page + ")"));
         for (Map.Entry<Integer, ItemStack> item : items.entrySet()) {
             inventory.setItem(item.getKey(), item.getValue());
@@ -56,7 +65,7 @@ public class EventInventory implements Listener {
     /**
      * Returns a list of quests sorted by level, adjust for player class, and other parameters
      *
-     * @param player
+     * @param player               to sort list for
      * @param showRepeatableQuests whether to display repeatable quests or not
      * @return a list of sorted quests
      */
@@ -99,6 +108,55 @@ public class EventInventory implements Listener {
         return sorted;
     }
 
+    private static ItemStack backButton() {
+        ItemStack back = new ItemStack(Material.LIGHT_GRAY_STAINED_GLASS_PANE);
+        ItemMeta meta = back.getItemMeta();
+        assert meta != null;
+        meta.setDisplayName(ChatColor.RED + "Return");
+        meta.setLore(Collections.singletonList(ChatColor.GRAY + "Return to the first page"));
+        back.setItemMeta(meta);
+        return back;
+    }
+
+    private static ItemStack infoPaper() {
+        ItemStack infoPaper = new ItemStack(Material.PAPER);
+        ItemMeta meta = infoPaper.getItemMeta();
+        assert meta != null;
+        meta.setDisplayName(ChatColor.GOLD + "Quest menu");
+        meta.setLore(Arrays.asList
+                (
+                        ChatColor.GRAY + "Here you can view quests available to you!",
+                        "",
+                        ChatColor.GRAY + "" + ChatColor.BOLD + "KEY:",
+                        ChatColor.GOLD + "Gold" + ChatColor.GRAY + " quests are main story",
+                        ChatColor.YELLOW + "Yellow" + ChatColor.GRAY + " quests are side quests",
+                        ChatColor.AQUA + "Blue" + ChatColor.GRAY + " quests are repeatable",
+                        ChatColor.RED + "Red" + ChatColor.GRAY + " quests are missing requirements",
+                        ChatColor.GREEN + "Green" + ChatColor.GRAY + " quests are complete"
+                ));
+        infoPaper.setItemMeta(meta);
+        return infoPaper;
+    }
+
+    private static ItemStack toggleShowRepeatableQuestsItem() {
+        ItemStack infoPaper = new ItemStack(Material.LIGHT_BLUE_DYE);
+        ItemMeta meta = infoPaper.getItemMeta();
+        assert meta != null;
+        meta.setDisplayName(ChatColor.GOLD + "Toggle Repeatable Quests");
+        meta.setLore(Collections.singletonList(ChatColor.GRAY + "If toggled, only repeatable quests will be shown!"));
+        infoPaper.setItemMeta(meta);
+        return infoPaper;
+    }
+
+    private static ItemStack forwardArrow() {
+        ItemStack arrow = new ItemStack(Material.BROWN_STAINED_GLASS_PANE);
+        ItemMeta meta = arrow.getItemMeta();
+        assert meta != null;
+        meta.setDisplayName(ChatColor.GOLD + "Next Page");
+        arrow.setItemMeta(meta);
+        return arrow;
+    }
+
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
         playersInQuestGui.remove(event.getPlayer().getUniqueId());
@@ -124,46 +182,7 @@ public class EventInventory implements Listener {
             openQuestGui(player, playersInQuestGui.get(uuid).getCurrentPage() + 1);
         } else if (event.getCurrentItem().equals(toggleShowRepeatableQuestsItem())) {
             playersInQuestGui.get((uuid)).setShowingRepeatableQuests(!playersInQuestGui.get(uuid).isShowingRepeatableQuests());
-            openQuestGui(player, playersInQuestGui.get(uuid).getCurrentPage());
+            openQuestGui(player, 1);
         }
-    }
-
-    private static ItemStack backButton() {
-        ItemStack back = new ItemStack(Material.LIGHT_GRAY_STAINED_GLASS_PANE);
-        ItemMeta meta = back.getItemMeta();
-        assert meta != null;
-        meta.setDisplayName(ChatColor.RED + "Return");
-        meta.setLore(Collections.singletonList(ChatColor.GRAY + "Return to the first page"));
-        back.setItemMeta(meta);
-        return back;
-    }
-
-    private static ItemStack infoPaper() {
-        ItemStack infoPaper = new ItemStack(Material.PAPER);
-        ItemMeta meta = infoPaper.getItemMeta();
-        assert meta != null;
-        meta.setDisplayName(ChatColor.GOLD + "Quest menu");
-        meta.setLore(Collections.singletonList(ChatColor.GRAY + "Visit the next page"));
-        infoPaper.setItemMeta(meta);
-        return infoPaper;
-    }
-
-    private static ItemStack toggleShowRepeatableQuestsItem() {
-        ItemStack infoPaper = new ItemStack(Material.LIGHT_BLUE_DYE);
-        ItemMeta meta = infoPaper.getItemMeta();
-        assert meta != null;
-        meta.setDisplayName(ChatColor.GOLD + "Toggle Repeatable Quests");
-        meta.setLore(Collections.singletonList(ChatColor.GRAY + "If toggled, only repeatable quests will be shown!"));
-        infoPaper.setItemMeta(meta);
-        return infoPaper;
-    }
-
-    private static ItemStack forwardArrow() {
-        ItemStack arrow = new ItemStack(Material.BROWN_STAINED_GLASS_PANE);
-        ItemMeta meta = arrow.getItemMeta();
-        assert meta != null;
-        meta.setDisplayName(ChatColor.GOLD + "Next Page");
-        arrow.setItemMeta(meta);
-        return arrow;
     }
 }
