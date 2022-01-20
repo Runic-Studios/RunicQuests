560bbb906ebab0901dc751b3bc011619fda63ede
skyfallin <dimetrius.hightower@gmail.com>
2022-01-20 15:56:29 -0700

    removed hotbar messages as they are now unnecessary. fixed idle message bug!

--- src/main/java/com/runicrealms/runicquests/event/EventPlayerLocation.java
+++ src/main/java/com/runicrealms/runicquests/event/EventPlayerLocation.java
@@ -11,8 +11,6 @@ import com.runicrealms.runicquests.quests.QuestObjectiveType;
 import com.runicrealms.runicquests.quests.objective.QuestObjective;
 import com.runicrealms.runicquests.task.TaskQueue;
 import com.runicrealms.runicquests.util.RunicCoreHook;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
@@ -81,7 +79,6 @@ public class EventPlayerLocation implements Listener {
                     runnables.add(() -> {
                         player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&l&6New objective for: &r&l&e") + quest.getQuestName());
                         player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&e- &r&6" + goalMessage));
-                        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.YELLOW + goalMessage));
                         if (objective.shouldDisplayNextObjectiveTitle())
                             player.sendTitle(ChatColor.GOLD + "New Objective", ChatColor.YELLOW + goalMessage, 10, 80, 10); // Send a goal message title
                         Plugin.updatePlayerCachedLocations(player);
@@ -91,7 +88,6 @@ public class EventPlayerLocation implements Listener {
                 } else {
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&l&6New objective for: &r&l&e") + quest.getQuestName());
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&e- &r&6" + goalMessage));
-                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.YELLOW + goalMessage));
                     if (objective.shouldDisplayNextObjectiveTitle())
                         player.sendTitle(ChatColor.GOLD + "New Objective", ChatColor.YELLOW + goalMessage, 10, 80, 10); // Send a goal message title
                     Plugin.updatePlayerCachedLocations(player);
@@ -124,7 +120,6 @@ public class EventPlayerLocation implements Listener {
                         player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&a- &r" + quest.getRewards().getMoneyReward() + " &r&aCoin" + (quest.getRewards().getMoneyReward() == 1 ? "" : "s")));
                     if (quest.getRewards().getExperienceReward() != 0)
                         player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&a- &r" + quest.getRewards().getExperienceReward() + " &r&aExperience Point" + (quest.getRewards().getExperienceReward() == 1 ? "" : "s")));
-                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.YELLOW + "Quest Complete!"));
                     if (objective.shouldDisplayNextObjectiveTitle())
                         player.sendTitle(ChatColor.GOLD + "Quest Complete!", ChatColor.YELLOW + quest.getQuestName(), 10, 80, 10); // Send a goal message title
                 }
