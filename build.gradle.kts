ca327ebe05df131ceac9a245d221680b8109c769
Excel619 <excel8392@gmail.com>
2023-05-23 11:18:36 -0600

    Updated build.gradle.kts to match superproject

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
+val artifactName = "quests"
+val rrGroup: String by rootProject.extra
+val rrVersion: String by rootProject.extra
+
 plugins {
     `java-library`
     `maven-publish`
 }
 
-group = "com.runicrealms.plugin"
-version = "1.0-SNAPSHOT"
-val artifactName = "quests"
+group = rrGroup
+version = rrVersion
 
 dependencies {
     compileOnly(commonLibs.taskchain)
@@ -36,9 +39,9 @@ java {
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            groupId = "com.runicrealms.plugin"
+            groupId = rrGroup
             artifactId = artifactName
-            version = "1.0-SNAPSHOT"
+            version = rrVersion
             from(components["java"])
         }
     }
